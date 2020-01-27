6de4f1174e472825933619ef98c0e87435049aea
andrey <andreibessel@yahoo.com>
2020-01-27 17:51:15 +0200

    hi

---
+++ Database/src/mysql/mysql.java
@@ -0,0 +1,10 @@
+package mysql;
+
+public class mysql {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Database/src/odbc/SQLdataBase.java
@@ -0,0 +1,34 @@
+package odbc;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class SQLdataBase {
+
+private Connection con;
+private Statement st;
+private static final String url="jdbc:odbc://xxx.xxx.xxx.xxx:3306";
+private static final String className="sun.jdbc.odbc.JdbcOdbcDriver";
+
+    private static  String user;
+private static  String pass;
+
+ SQLdataBase(String userName, String password) {
+           user=userName;
+        pass=password;
+    try {
+            Class.forName(className);
+ con = DriverManager.getConnection(url, user, pass);
+ System.out.println("success");
+            st = con.createStatement();
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+  //do whatever database processing is required
+ }
+
+    public void queryNoReturn(String query) throws SQLException{
+        st.executeQuery(query);
+    }
+
+ }
