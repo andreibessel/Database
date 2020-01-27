c030c409b6198db33730998c66316724f296ccc9
andrey <andreibessel@yahoo.com>
2020-01-27 17:56:13 +0200

    hi2

--- Database/src/odbc/SQLdataBase.java
+++ Database/src/odbc/SQLdataBase.java
@@ -1,6 +1,8 @@
 package odbc;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class SQLdataBase {
@@ -11,15 +13,15 @@ private static final String url="jdbc:odbc://xxx.xxx.xxx.xxx:3306";
 private static final String className="sun.jdbc.odbc.JdbcOdbcDriver";
 
     private static  String user;
-private static  String pass;
+    private static  String pass;
 
  SQLdataBase(String userName, String password) {
            user=userName;
         pass=password;
     try {
             Class.forName(className);
- con = DriverManager.getConnection(url, user, pass);
- System.out.println("success");
+            con = DriverManager.getConnection(url, user, pass);
+            System.out.println("success");
             st = con.createStatement();
         } catch (Exception ex) {
             System.out.println(ex);
@@ -27,6 +29,7 @@ private static  String pass;
   //do whatever database processing is required
  }
 
+ 
     public void queryNoReturn(String query) throws SQLException{
         st.executeQuery(query);
     }
